fab4f41e38926db4f8b058ee9ebdbec72fa6b3a6
root <root@0b41bd5f873c.mylabserver.com>
2026-05-10 12:10:43 +0000

    feat: fixed S3 state bucket name

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
       backend "s3" {
-         bucket = "rs7863121991"
+         bucket = "rs07863121991"
          key = "state/terraform.tfstate"
          region = "us-east-1"
       }
